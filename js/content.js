1baec0e86ce2bd0c82436c856f3cdc307e012ce1
unknown <you@example.com>
2021-02-22 13:50:58 -0700

    converted come css code to js

--- js/content.js
+++ js/content.js
@@ -6,3 +6,30 @@ console.log("extension is running!")
 
 // TRY: using a mouseenter event listener to update text to something new.
 
+$("body").css("background-color", "#171617");
+$("body").css("color", "white");
+
+$(".navbar-light .navbar-toggler").css("border", "1px solid #343434");
+
+$(".card").css("background-color", "#343434");
+
+//$(".calendar_event_course").css("background-color", "#7D7A7C");
+
+$(".btn").css("background-color", "#363636");
+$(".btn").css("color", "white");
+$(".btn").css("border-color", "#7D7A7C");
+
+$(".uleth_main_top_nav").css("background-color", "#171617");
+
+$("#uleth_burgers_fries_n_pop.navbar-collapse.collapse.show").css("border-color", "#7D7A7C");
+
+$(".dropdown-menu.dropdown-menu-right.menu.align-tr-br.show").css("background-color", "#363636");
+
+$(".page-link").css("background-color", "#363636");
+$(".page-link").css("color", "#007193");
+
+$("#region-main").css("background-color", "#171617");
+
+$(".generaltable").css("color", "#B3B1B3");
+
+$(".course-content ul.ctopics li.section .content .toggle span, .course-content ul.ctopics li.section .content.sectionhidden").css("background-color", "#363636");
