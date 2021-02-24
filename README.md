46e236a2766ff6ce14452d148277b442f14f6a15
unknown <you@example.com>
2021-02-23 17:29:00 -0700

    Added extra code And Read Me Info

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-This is the READ ME
+#H1 University of Lethbridge Moodle Dark Mode Extension
+
+##H2 What This Extension Does:
+
+This extension is just a simple dark mode that many websites use, such as Youtube or GitHub. This extension brings dark mode into the University of Lethbridge moodle website. Allowing users to see the site in a more darker apperance that's a little easier on the eyes. The exntension should work on all current and previous classes the user may have and should hopefully work on all areas of the moodle site the user has access to.
+
+##H2 Copyright
+
+University of Lethbrige logo (c) to the University of Lethbridge
--- js/content.js
+++ js/content.js
@@ -1,10 +1,5 @@
 console.log("extension is running!")
 
-// TRY: updating the text content of an element or set of elements on one or more pages
-
-// TRY: adding a click event listener to an element that changes the background of that element to a new color
-
-// TRY: using a mouseenter event listener to update text to something new.
 
 $("body").css("background-color", "#171617");
 $("body").css("color", "white");
@@ -35,9 +30,7 @@ $(".generaltable").css("color", "#B3B1B3");
 $(" .content.sectionhidden, .sectionhead.toggle.toggle-point").css("background-color", "#363636");
 $(" .sectionname").css("color", "#007193");
 
-//$(".hidden").css("background-color", "#363636");
-//$(".hidden").css("color", "white");
-
+$(".modal-content, .moodle-dialogue-base .moodle-dialogue-wrap.moodle-dialogue-content").css("background-color", "#363636");
 
 //image credit: University of Lethbridge, edited by myself
 var imgURL = chrome.runtime.getURL("images/university_logoEdit.png");
